13fad8bfa7ada7057050c208d028626800dba2fb
Mihei03 <70269913+Mihei03@users.noreply.github.com>
2024-03-18 03:28:31 +0700

    ?

--- obj/Debug/net7.0-windows/App.g.i.cs
+++ obj/Debug/net7.0-windows/App.g.i.cs
@@ -51,7 +51,7 @@ namespace CompilerDemo {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -63,7 +63,7 @@ namespace CompilerDemo {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/CompilerDemo;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CompilerDemo;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -77,7 +77,7 @@ namespace CompilerDemo {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public static void Main() {
             CompilerDemo.App app = new CompilerDemo.App();
             app.InitializeComponent();
