bf0a77ae172314ea7a1a5497e07efdcb54f4f4a0
Mihei03 <70269913+Mihei03@users.noreply.github.com>
2024-03-31 23:27:18 +0700

    Проверка

--- obj/Debug/net7.0-windows/CompilerDemo.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/CompilerDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompilerDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6b3bde522fe0c4dfc1aaac74fc716808575897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebd8727c9713ca9a7ea17b6e1712466fff03b51")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompilerDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompilerDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
